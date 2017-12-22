60e815bc3f0f24623ed209cd0fdf35e2eb513eee
Dain <thedazaar@gmail.com>
2017-12-22 12:15:17 -0700

    Implement toString for EncodedStringValue

--- src/main/java/com/google/android/mms/pdu_alt/EncodedStringValue.java
+++ src/main/java/com/google/android/mms/pdu_alt/EncodedStringValue.java
@@ -277,4 +277,9 @@ public class EncodedStringValue implements Cloneable {
         }
         return null;
     }
+
+    @Override
+    public String toString() {
+        return getString();
+    }
 }
